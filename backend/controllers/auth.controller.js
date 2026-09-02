36b7f13efd6c708ba9eb97cb08bf9d818ff3548b
Kams <117517515+benikams03@users.noreply.github.com>
2026-09-02 11:21:19 +0100

    feat: route et conroller ou auth de compte simulation

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -151,6 +151,93 @@ class AuthController {
     // }
 
 
+
+
+
+    // connexion et creation de compte du coter de la partie simulation
+    async registerSimulation(req, reply) {  
+        try{
+            
+            const { email, password } = req.body;
+        
+            const salt = await bcrypt.genSalt(5)
+            const password_hash = await bcrypt.hash(password, salt)
+
+            const verify_email = await this.users.findOne({
+                email: email.toLowerCase()
+            })
+
+            if ( verify_email ) {
+                await logsService.createLogCreationCompte('error', email.toLowerCase(), 'simulation', req.ip)
+                return reply.send({
+                    success: false,
+                    message: "Votre adresse email est déjà utilisée",
+                })
+            }
+
+            const result = await this.users.insertOne({
+                email: email.toLowerCase(),
+                password: password_hash,
+                role: 'simulation'
+            })
+            
+            await logsService.createLogCreationCompte('success', email.toLowerCase(), 'simulation', req.ip)
+
+            reply.send({
+                success: true
+            })
+
+        } catch (error) {
+            reply.send({
+                success: false,
+                message: error.message,
+            })
+        }
+    }
+
+    async loginSimulation( req, reply) {
+        try {
+            const { email, password } = req.body;
+            
+            const user = await this.users.findOne({
+                email: email.toLowerCase(),
+                role: 'simulation'
+            });
+
+            if (!user) {
+                await logsService.createLogConnexion('error', email.toLowerCase(), 'simulation', req.ip)
+
+                return reply.send({
+                    success: false,
+                    message: "Adresse email ou mot de passe incorrect",
+                })
+            }
+
+            const isPasswordValid = await bcrypt.compare(password, user.password);
+
+            if (!isPasswordValid) {
+                await logsService.createLogConnexion('error', email.toLowerCase(), 'simulation', req.ip)
+                
+                return reply.send({
+                    success: false,
+                    message: "Adresse email ou mot de passe incorrect",
+                })
+            }
+
+            await logsService.createLogConnexion('success', email.toLowerCase(), 'simulation', req.ip)
+            
+            reply.send({
+                success: true
+            });
+
+        } catch (error) {
+            reply.send({
+                success: false,
+                message: error.message,
+            })
+        }
+    }
+
 }
 
 export default new AuthController();
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,6 +5,10 @@ export default function authRoutes (app) {
 
     app.post('/register', (req, reply) => authController.register(app, req, reply) )
     app.post('/login', (req, reply) => authController.login(app, req, reply) )
+
+    app.post('/register-simulation', (req, reply) => authController.registerSimulation(req, reply) )
+    app.post('/login-simulation', (req, reply) => authController.loginSimulation(req, reply) )
+
     app.get('/logs', (req, reply) => logsController.get(req, reply) )
 
 
